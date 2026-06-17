78c079d46e3dccd51bbf2d1abbbbe5ee5041c850
kareemanwar <kareemahmedanwar@gmail.com>
2026-06-17 18:53:31 +0300

    Align sources navbar new action with native behavior

--- ui/v2.5/src/components/MainNavbar.tsx
+++ ui/v2.5/src/components/MainNavbar.tsx
@@ -150,6 +150,7 @@ const allMenuItems: IMenuItem[] = [
     href: "/sources",
     icon: faVideo,
     hotkey: "g o",
+    userCreatable: true,
   },
   {
     name: "performers",
@@ -287,11 +288,7 @@ export const MainNavbar: React.FC = () => {
   );
 
   const pathname = location.pathname.replace(/\/$/, "");
-  let newPath = newPathsList.includes(pathname)
-    ? `${pathname}/new`
-    : pathname === "/sources"
-      ? "/sources/new"
-      : null;
+  let newPath = newPathsList.includes(pathname) ? `${pathname}/new` : null;
   if (newPath !== null) {
     const queryParam = new URLSearchParams(location.search).get("q");
     if (queryParam) {

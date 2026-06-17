bd9812a470b9836156e639071255ce8a00480175
kareemanwar <kareemahmedanwar@gmail.com>
2026-06-17 09:46:52 +0300

    Split resolver helper implementations

--- internal/api/resolver.go
+++ internal/api/resolver.go
@@ -3,29 +3,18 @@ package api
 import (
 	"context"
 	"errors"
-	"fmt"
-	"sort"
-	"strconv"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/stashapp/stash/internal/build"
 	"github.com/stashapp/stash/internal/manager"
-	"github.com/stashapp/stash/pkg/logger"
 	"github.com/stashapp/stash/pkg/models"
 	"github.com/stashapp/stash/pkg/plugin/hook"
 	"github.com/stashapp/stash/pkg/scraper"
 )
 
 var (
-	// ErrNotImplemented is an error which means the given functionality isn't implemented by the API.
 	ErrNotImplemented = errors.New("not implemented")
-
-	// ErrNotSupported is returned whenever there's a test, which can be used to guard against the error,
-	// but the given parameters aren't supported by the system.
-	ErrNotSupported = errors.New("not supported")
-
-	// ErrInput signifies errors where the input isn't valid for some reason. And no more specific error exists.
-	ErrInput = errors.New("input error")
+	ErrNotSupported   = errors.New("not supported")
+	ErrInput          = errors.New("input error")
 )
 
 type hookExecutor interface {
@@ -38,90 +27,37 @@ type Resolver struct {
 	imageService   manager.ImageService
 	galleryService manager.GalleryService
 	groupService   manager.GroupService
-
-	hookExecutor hookExecutor
+	hookExecutor   hookExecutor
 }
 
-func (r *Resolver) scraperCache() *scraper.Cache {
-	return manager.GetInstance().ScraperCache
-}
-
-func (r *Resolver) Gallery() GalleryResolver {
-	return &galleryResolver{r}
-}
-func (r *Resolver) GalleryChapter() GalleryChapterResolver {
-	return &galleryChapterResolver{r}
-}
-func (r *Resolver) Mutation() MutationResolver {
-	return &mutationResolver{r}
-}
-func (r *Resolver) Performer() PerformerResolver {
-	return &performerResolver{r}
-}
-func (r *Resolver) Query() QueryResolver {
-	return &queryResolver{r}
-}
-func (r *Resolver) Scene() SceneResolver {
-	return &sceneResolver{r}
-}
-func (r *Resolver) ScrapedScene() ScrapedSceneResolver {
-	return &scrapedSceneResolver{r}
-}
-func (r *Resolver) Source() SourceResolver {
-	return &sourceResolver{r}
-}
-func (r *Resolver) Image() ImageResolver {
-	return &imageResolver{r}
-}
-func (r *Resolver) SceneMarker() SceneMarkerResolver {
-	return &sceneMarkerResolver{r}
-}
-func (r *Resolver) Studio() StudioResolver {
-	return &studioResolver{r}
-}
-
-func (r *Resolver) Group() GroupResolver {
-	return &groupResolver{r}
-}
-func (r *Resolver) Movie() MovieResolver {
-	return &movieResolver{&groupResolver{r}}
-}
-
-func (r *Resolver) Subscription() SubscriptionResolver {
-	return &subscriptionResolver{r}
-}
-func (r *Resolver) Tag() TagResolver {
-	return &tagResolver{r}
-}
-func (r *Resolver) GalleryFile() GalleryFileResolver {
-	return &galleryFileResolver{r}
-}
-func (r *Resolver) VideoFile() VideoFileResolver {
-	return &videoFileResolver{r}
-}
-func (r *Resolver) ImageFile() ImageFileResolver {
-	return &imageFileResolver{r}
-}
-func (r *Resolver) BasicFile() BasicFileResolver {
-	return &basicFileResolver{r}
-}
-func (r *Resolver) Folder() FolderResolver {
-	return &folderResolver{r}
-}
-func (r *Resolver) SavedFilter() SavedFilterResolver {
-	return &savedFilterResolver{r}
-}
-func (r *Resolver) Plugin() PluginResolver {
-	return &pluginResolver{r}
-}
-func (r *Resolver) ConfigResult() ConfigResultResolver {
-	return &configResultResolver{r}
-}
+func (r *Resolver) scraperCache() *scraper.Cache { return manager.GetInstance().ScraperCache }
+func (r *Resolver) Gallery() GalleryResolver { return &galleryResolver{r} }
+func (r *Resolver) GalleryChapter() GalleryChapterResolver { return &galleryChapterResolver{r} }
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+func (r *Resolver) Performer() PerformerResolver { return &performerResolver{r} }
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+func (r *Resolver) Scene() SceneResolver { return &sceneResolver{r} }
+func (r *Resolver) ScrapedScene() ScrapedSceneResolver { return &scrapedSceneResolver{r} }
+func (r *Resolver) Source() SourceResolver { return &sourceResolver{r} }
+func (r *Resolver) Image() ImageResolver { return &imageResolver{r} }
+func (r *Resolver) SceneMarker() SceneMarkerResolver { return &sceneMarkerResolver{r} }
+func (r *Resolver) Studio() StudioResolver { return &studioResolver{r} }
+func (r *Resolver) Group() GroupResolver { return &groupResolver{r} }
+func (r *Resolver) Movie() MovieResolver { return &movieResolver{&groupResolver{r}} }
+func (r *Resolver) Subscription() SubscriptionResolver { return &subscriptionResolver{r} }
+func (r *Resolver) Tag() TagResolver { return &tagResolver{r} }
+func (r *Resolver) GalleryFile() GalleryFileResolver { return &galleryFileResolver{r} }
+func (r *Resolver) VideoFile() VideoFileResolver { return &videoFileResolver{r} }
+func (r *Resolver) ImageFile() ImageFileResolver { return &imageFileResolver{r} }
+func (r *Resolver) BasicFile() BasicFileResolver { return &basicFileResolver{r} }
+func (r *Resolver) Folder() FolderResolver { return &folderResolver{r} }
+func (r *Resolver) SavedFilter() SavedFilterResolver { return &savedFilterResolver{r} }
+func (r *Resolver) Plugin() PluginResolver { return &pluginResolver{r} }
+func (r *Resolver) ConfigResult() ConfigResultResolver { return &configResultResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
 type galleryResolver struct{ *Resolver }
 type galleryChapterResolver struct{ *Resolver }
 type performerResolver struct{ *Resolver }
@@ -131,11 +67,8 @@ type sourceResolver struct{ *Resolver }
 type sceneMarkerResolver struct{ *Resolver }
 type imageResolver struct{ *Resolver }
 type studioResolver struct{ *Resolver }
-
-// movie is group under the hood
 type groupResolver struct{ *Resolver }
 type movieResolver struct{ *groupResolver }
-
 type tagResolver struct{ *Resolver }
 type galleryFileResolver struct{ *Resolver }
 type videoFileResolver struct{ *Resolver }
@@ -146,131 +79,12 @@ type savedFilterResolver struct{ *Resolver }
 type pluginResolver struct{ *Resolver }
 type configResultResolver struct{ *Resolver }
 
-func (r *Resolver) withTxn(ctx context.Context, fn func(ctx context.Context) error) error {
-	return r.repository.WithTxn(ctx, fn)
-}
-
-func (r *Resolver) withReadTxn(ctx context.Context, fn func(ctx context.Context) error) error {
-	return r.repository.WithReadTxn(ctx, fn)
-}
-
-// idOnly returns true if the query is only asking for the id field.
-// This can be used to optimize certain queries where we don't need to load the full object if we're only getting the id.
+func (r *Resolver) withTxn(ctx context.Context, fn func(ctx context.Context) error) error { return r.repository.WithTxn(ctx, fn) }
+func (r *Resolver) withReadTxn(ctx context.Context, fn func(ctx context.Context) error) error { return r.repository.WithReadTxn(ctx, fn) }
 func (r *Resolver) idOnly(ctx context.Context) bool {
 	fields := graphql.CollectAllFields(ctx)
 	return len(fields) == 1 && fields[0] == "id"
 }
-
 func (r *scrapedSceneResolver) OnlineMedia(ctx context.Context, obj *models.ScrapedScene) (*ScrapedSceneOnlineMedia, error) {
 	return scrapedSceneOnlineMediaForScene(obj)
-}
-
-func (r *queryResolver) MarkerWall(ctx context.Context, q *string) (ret []*models.SceneMarker, err error) {
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		ret, err = r.repository.SceneMarker.Wall(ctx, q)
-		return err
-	}); err != nil {
-		return nil, err
-	}
-	return ret, nil
-}
-
-func (r *queryResolver) SceneWall(ctx context.Context, q *string) (ret []*models.Scene, err error) {
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		ret, err = r.repository.Scene.Wall(ctx, q)
-		return err
-	}); err != nil {
-		return nil, err
-	}
-
-	return ret, nil
-}
-
-func (r *queryResolver) MarkerStrings(ctx context.Context, q *string, sort *string) (ret []*models.MarkerStringsResultType, err error) {
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		ret, err = r.repository.SceneMarker.GetMarkerStrings(ctx, q, sort)
-		return err
-	}); err != nil {
-		return nil, err
-	}
-
-	return ret, nil
-}
-
-func (r *queryResolver) Stats(ctx context.Context) (*StatsResultType, error) {
-	var ret StatsResultType
-	if err := r.withReadTxn(ctx, func(ctx context.Context) error {
-		repo := r.repository
-		sceneQB := repo.Scene
-		imageQB := repo.Image
-		galleryQB := repo.Gallery
-		studioQB := repo.Studio
-		performerQB := repo.Performer
-		movieQB := repo.Group
-		tagQB := repo.Tag
-
-		// embrace the error
-
-		scenesCount, err := sceneQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		scenesSize, err := sceneQB.Size(ctx)
-		if err != nil {
-			return err
-		}
-
-		scenesDuration, err := sceneQB.Duration(ctx)
-		if err != nil {
-			return err
-		}
-
-		imagesCount, err := imageQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		galleriesCount, err := galleryQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		studiosCount, err := studioQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		performersCount, err := performerQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		moviesCount, err := movieQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		tagsCount, err := tagQB.Count(ctx)
-		if err != nil {
-			return err
-		}
-
-		ret.Scenes = scenesCount
-		ret.ScenesSize = scenesSize
-		ret.ScenesDuration = scenesDuration
-		ret.Images = imagesCount
-		ret.Galleries = galleriesCount
-		ret.Studios = studiosCount
-		ret.Performers = performersCount
-		ret.Movies = moviesCount
-		ret.Groups = moviesCount
-		ret.Tags = tagsCount
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return &ret, nil
 }

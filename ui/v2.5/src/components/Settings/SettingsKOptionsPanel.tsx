cb97184c823ba3bf0f425f1ee7f2ad763a792ba0
kareemanwar <kareemahmedanwar@gmail.com>
2026-06-17 03:04:53 +0300

    Add online scenes K-option

--- ui/v2.5/src/components/Settings/SettingsKOptionsPanel.tsx
+++ ui/v2.5/src/components/Settings/SettingsKOptionsPanel.tsx
@@ -8,12 +8,13 @@ export const SettingsKOptionsPanel: React.FC = () => {
 
   const kOptions = ui.kOptions ?? {};
   const showTestTab = kOptions.showScraperTestTab ?? true;
+  const enableOnlineScenes = kOptions.enableOnlineScenes ?? true;
 
-  function setShowTestTab(show: boolean) {
+  function saveKOptions(next: typeof kOptions) {
     saveUI({
       kOptions: {
         ...kOptions,
-        showScraperTestTab: show,
+        ...next,
       },
     });
   }
@@ -22,6 +23,7 @@ export const SettingsKOptionsPanel: React.FC = () => {
     <SettingSection id="k-options">
       <h1>K-Options</h1>
       <div className="sub-heading">Stash-a feature switches.</div>
+
       <div className="setting-section" id="k-options-test-tab">
         <div className="setting">
           <div>
@@ -34,7 +36,28 @@ export const SettingsKOptionsPanel: React.FC = () => {
             id="k-options-show-test-tab"
             type="switch"
             checked={showTestTab}
-            onChange={(event) => setShowTestTab(event.currentTarget.checked)}
+            onChange={(event) =>
+              saveKOptions({ showScraperTestTab: event.currentTarget.checked })
+            }
+          />
+        </div>
+      </div>
+
+      <div className="setting-section" id="k-options-online-scenes">
+        <div className="setting">
+          <div>
+            <h3>Online scenes</h3>
+            <div className="sub-heading">
+              Enable online scene imports and playback experiments.
+            </div>
+          </div>
+          <Form.Check
+            id="k-options-enable-online-scenes"
+            type="switch"
+            checked={enableOnlineScenes}
+            onChange={(event) =>
+              saveKOptions({ enableOnlineScenes: event.currentTarget.checked })
+            }
           />
         </div>
       </div>

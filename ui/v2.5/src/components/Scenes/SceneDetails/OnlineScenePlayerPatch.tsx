cf9b3ea55d00d31a433c13c19dcb6fc5d6d6aff0
kareemanwar <kareemahmedanwar@gmail.com>
2026-06-17 05:33:52 +0300

    Register scene playback patch

--- ui/v2.5/src/components/Scenes/SceneDetails/OnlineScenePlayerPatch.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/OnlineScenePlayerPatch.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
+import { instead } from "src/patch";
 
-const OnlineScenePlayerPatch: React.FC = () => null;
+interface IScenePlayerPatchProps {
+  scene: {
+    id: string;
+    files: unknown[];
+  };
+}
 
-export default OnlineScenePlayerPatch;
+const OnlineScenePlayer: React.FC<{ sceneID: string }> = ({ sceneID }) => {
+  return <Alert variant="info">Online scene player: {sceneID}</Alert>;
+};
+
+instead(
+  "ScenePlayer",
+  (props: IScenePlayerPatchProps, next: React.FC<IScenePlayerPatchProps>) => {
+    if (props.scene.files.length === 0) {
+      return <OnlineScenePlayer sceneID={props.scene.id} />;
+    }
+
+    return next(props);
+  }
+);
+
+export default OnlineScenePlayer;
